f34306b62c1218da5b0400d1f8ad0354d67ed81c
AAKRATI GUPTA <107616649+aakrati442@users.noreply.github.com>
2023-08-07 12:26:46 +0530

    Create upto operators.java

---
+++ upto operators.java
@@ -0,0 +1,55 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		//System.out.println("Hello World");
+		
+		
+		//variables
+// 		String name="AA";
+// 	    System.out.println(name);	
+// 	    int a=20;
+// 	    float b=45.22f;
+// 	    boolean isAdult=false;
+// 	    System.out.println(a);
+// 	    System.out.println(b);
+// 	    System.out.println(isAdult);
+	    
+	    
+	    //datatypes in java --- primitive and reference 
+	   // byte u=56;
+	   // double d=56.6794567d;
+	   // System.out.println(d);
+	   // char grade='A';
+	   // System.out.println((int)grade);
+	    
+	   // String h="harry";
+	   // System.out.println(h.charAt(0));
+	    
+	    
+	    //operators in java
+	    //operand, operator
+	    
+	    //Arithmetic operator
+	   // int n1=45, n2=54;
+       //System.out.println(n1+n2);
+	   // System.out.println(n1-n2);
+	   // System.out.println(n1*n2);
+	   // System.out.println(n1/n2);
+	   // System.out.println(n1%n2);
+	    
+	    //increment and decrement
+	   // System.out.println(n1++);
+	   // System.out.println(n1--);
+	   // System.out.println(++n1);
+	   // System.out.println(--n1);
+	    
+	    //comparison operators
+	    int a=5,b=4;
+	    System.out.println(a==b);   //<=,>=,<,>,==,!=
+	    
+	    //logical operators
+	    // &&, ||, !
+	    
+	}
+}
