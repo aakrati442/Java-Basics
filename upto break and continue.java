eb5ab164c377ffc2b70a09a7e233d4b2f6341acf
AAKRATI GUPTA <107616649+aakrati442@users.noreply.github.com>
2023-08-07 18:53:27 +0530

    Create upto break and continue.java

---
+++ upto break and continue.java
@@ -0,0 +1,79 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    
+	    //String methods
+	    
+	   // String name="aa";
+	   // String lname="gupta";
+	    
+		//System.out.println(name+" Hello "+lname);   //concat
+// 		System.out.println(name.length());
+// 		System.out.println(name.toUpperCase());
+// 		System.out.println(name.toLowerCase());
+// 		System.out.println(name+" from\" "+lname);
+// 		System.out.println(name+" from\\ "+lname);
+// 		System.out.println(name+" from\t "+lname);
+// 		System.out.println(name+" from\n "+lname);
+
+        //System.out.println(name.contains("aa"));
+        // System.out.println(name.charAt(2));
+        //System.out.println(name.endsWith("x"));
+        //System.out.println(name.indexOf("tt"));
+        
+        
+        //Math class in java
+        
+//         int n1=5;
+//         int n2=4;
+//         System.out.println(Math.max(4,7));
+//         System.out.println(Math.min(n1,n2));
+//         System.out.println(Math.sqrt(36));
+//         System.out.println(Math.abs(-6));
+
+            // System.out.println(Math.random());
+            // System.out.println(4+(8-4)*Math.random());    //generates random no. between 8 and 4
+            
+            
+            //Switch statements
+            
+            // Scanner sc=new Scanner(System.in);
+            // int x=sc.nextInt();
+            // switch(x){
+            //     case 12:System.out.println("Hey");
+            //     break;
+            //     case 33: System.out.println("hi");
+            //     break;
+            //     default: System.out.println("didnt match");
+            // }
+            
+           
+           //loops
+           
+        //   int x1=0;
+        //   while(x1<100){
+        //       System.out.println(x1);
+        //       x1++;
+        //   }
+           
+        //   int x2=0;
+        //   do{
+        //       System.out.println(x2);
+        //       x2+=1;
+        //   }while(x2<100);
+           
+        //   for(int i=0;i<=100;i++){
+        //       System.out.println(i);
+        //   }
+        
+        
+	}
+}
