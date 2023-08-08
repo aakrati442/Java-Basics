fe1ad3b9869f55f0e757c2c84371d693b7b685cf
AAKRATI GUPTA <107616649+aakrati442@users.noreply.github.com>
2023-08-08 15:04:15 +0530

    Create upto methods.java

---
+++ upto methods.java
@@ -0,0 +1,59 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+import java.util.*;
+public class Main
+{
+    
+    static int sum(int a,int b){
+        return a+b;
+    }
+    
+	public static void main(String[] args) {
+		
+		
+		//java Array
+		
+// 		int marks[]= {1,2,3,4};
+// 		System.out.println(marks[0]);
+		
+		//marks[2]=5;
+		//for Each loop
+// 		for(int value:marks){
+// 		    System.out.println(value);
+// 		}
+		
+// 		int [][] m={{1,2,3},{4,5,6}};
+// 		System.out.println(m[0][1]);
+		
+// 		String arr[]={"aa","g","ag"};
+// 		for(String value:arr){
+// 		    System.out.println(value);
+// 		}
+		
+		
+		//try-catch in java
+// 		String arr[]={"aa","g","ag"};
+// 		try{
+// 		System.out.println(arr[3]);
+// 		}
+// 		catch(Exception e){
+// 		System.out.println(e);
+// 		}
+// 		finally{
+// 		    System.out.println("abc");
+// 		}
+		
+		
+		
+		//methods in java
+		System.out.println(sum(5,7));
+		
+		
+		//System.out.println("Hello World");
+	}
+}
